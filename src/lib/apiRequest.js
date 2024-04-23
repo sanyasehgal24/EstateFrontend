b47823f54e55bcac0c71c5078224907a331b6afb
Sanya Sehgal <sanyasehgal2405@gmail.com>
2024-04-24 04:47:20 +0530

    changes done

--- src/lib/apiRequest.js
+++ src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 // const port = process.env.PORT || 5173;
 const apiRequest = axios.create({
-  baseURL: "https://estatebackend-po3p.onrender.com",
+  baseURL: "https://estatebackend-po3p.onrender.com/",
   withCredentials: true,
 });
 
